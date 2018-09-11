4a05dd79c240a5bcfbf1cb9d84b2d23b93d6cc8c
Fouad OUAHI <fouad.client@gmail.com>
2018-09-10 20:55:22 -0400

    add authentication using database entries

--- src/main/java/org/fouad/cardatabase/config/SecurityConfig.java
+++ src/main/java/org/fouad/cardatabase/config/SecurityConfig.java
@@ -1,28 +1,21 @@
 package org.fouad.cardatabase.config;
 
-import org.springframework.context.annotation.Bean;
+import org.fouad.cardatabase.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	@Override
-	@SuppressWarnings("deprecation")
-	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()//
-		.username("user2")//
-		.password("password2")//
-		.roles("USER")//
-		.build();
-		return new InMemoryUserDetailsManager(userDetails);
-	}
+	  @Autowired
+	  public void configureGlobal(@Autowired UserDetailsServiceImpl userDetailsService,@Autowired AuthenticationManagerBuilder auth) throws Exception {
+	    auth.userDetailsService(userDetailsService)
+	    .passwordEncoder(new BCryptPasswordEncoder());
+	  }
 
 }
